2566f2eeec9f3b88d041aefbe949ba5baab8f0f8
Tudor BURA <bura.tudor@gmail.com>
2023-02-28 00:40:46 +0200

    Added standalone start scripts

---
+++ honeypots/standalone/start.sh
@@ -0,0 +1,12 @@
+apt-get install python3-pip python3-venv
+echo {\"logs\":\"terminal,url\", \"excluded_actions_from_logging\":[\"post\", \"get\", \"process\"], \"url\":\"$1\"} > config.json
+python3 -m venv .venv
+activate(){
+    . ./.venv/bin/activate
+    pip3 install -r requirements.txt
+    cp *.py .venv/lib/python3.9/site-packages/honeypots
+    honeypots --setup http:1234 --config config.json
+}
+activate
+
+#bash .venv/bin/activate && pip3 install -r requirements.txt && cp *.py .venv/lib/python3.9/site-packages/honeypots && echo {\"logs\":\"terminal,url\", \"url\":\"$1\"} > config.json && honeypots --setup http:1234 --config config.json
