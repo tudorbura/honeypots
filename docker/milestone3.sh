516d8867155279005c3f2522971bdb4ea482bd05
Tudor BURA <bura.tudor@gmail.com>
2023-03-01 15:47:35 +0200

    removed docker login

--- docker/milestone3.sh
+++ docker/milestone3.sh
@@ -6,10 +6,6 @@ sh get_docker.sh
 
 /etc/init.d/docker start
 
-#docker run -e URL=https://webhook.site/3e5bfa2f-1a52-47ea-ad6b-882beebe4e56 -e SERVICES=http:12345,ftp:123 -e EXCLUDED_ACTIONS=\"get\" -p 12345:12345 -p 123:123 -t -d honeypots-final
-#docker run -e URL=$1 -e SERVICES=$SERVICES -e EXCLUDED_ACTIONS=$EXCLUDED_ACTIONS -p 12345:12345 -p 123:123 -t -d honeypots-final
-
-
 # Setup desired listening ports
 HTTP_PORT=12345
 FTP_PORT=123
@@ -24,7 +20,7 @@ BIND_PORTS="-p $HTTP_PORT:$HTTP_PORT  -p $FTP_PORT:$FTP_PORT"
 EXCLUDED_ACTIONS=\"get\",\"post\",\"something\"
 
 #login step - will be removed
-docker login
+#docker login
 
 # run the image
 docker run -e URL=$1 -e SERVICES=$SERVICES -e EXCLUDED_ACTIONS=$EXCLUDED_ACTIONS $BIND_PORTS -it autorunnner00/my-images:start_all-1.0.0
